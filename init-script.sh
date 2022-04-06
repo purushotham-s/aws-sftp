ca748d6bccaeb6e8453872a0d38f24d1c6ea9527
Purushotham K S <purushotham.0608@gmail.com>
2022-04-06 12:41:16 -0400

    Updated init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -10,3 +10,6 @@ amazon-linux-extras install epel
 yum install s3fs-fuse awscli -y
 echo -e "sftp-srv-bucket-1\t/opt/sftp/incoming\tfuse.s3fs\t_netdev,allow_other,use_path_request_style,iam_role=ec2_s3_access_role,url=https://s3.us-east-1.amazonaws.com/\t0\t0" >> /etc/fstab
 mount -a
+pip3 install boto3
+wget https://raw.githubusercontent.com/purushotham-s/aws-sftp/main/manage-sftp-users.py -O /usr/local/sbin/manage-sftp-users
+chmod +x /usr/local/sbin/manage-sftp-users
