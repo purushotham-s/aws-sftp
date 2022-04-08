0b4cf879df70185dbe3aa32303e6031a11374da3
Purushotham K S <purushotham.0608@gmail.com>
2022-04-08 04:33:23 -0400

    Added output.tf to display EC2 IP and API URL

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "instance_public_ip" {
+  description = "SFTP EC2 instance Public IP address"
+  value       = aws_instance.sftp_ec2_instance.public_ip
+}
+
+output "api_url" {
+  description = "API URL"
+  value       = "https://${aws_api_gateway_rest_api.sftp_api.id}.execute-api.us-east-1.amazonaws.com/dev/sftp-data"
+}
